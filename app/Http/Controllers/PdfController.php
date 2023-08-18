3d2434b28524e5008f1ed3bc348e41d303f0b84f
alexisCobax <134947200+alexisCobax@users.noreply.github.com>
2023-08-17 21:05:32 -0300

    Feature/proforma (#8)
    * [feature] proforma datos
    
    * [feature] proforma
    
    * [featire invoice]
    
    ---------
    
    Co-authored-by: cynchro <alexissaucedo@gmail.com>

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,26 +3,35 @@
 namespace App\Http\Controllers;
 
 // use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+use App\Services\InvoiceService;
 use App\Services\ProformaService;
+use App\Transformers\Invoices\FindByIdTransformer;
+
 
 class PdfController extends Controller
 {
 
-    private $service;
+    private $proforma;
+    private $invoice;
 
-    public function __construct(ProformaService $ProformaService)
+    public function __construct(ProformaService $ProformaService, InvoiceService $InvoiceService)
     {
-        $this->service = $ProformaService;
+        $this->proforma = $ProformaService;
+        $this->invoice = $InvoiceService;
     }
 
     public function proforma(Request $request)
     {
-        return $this->service->findById($request);
+        return $this->proforma->findById($request);
     }
 
-    public function factura()
+
+    public function invoice(Request $request)
     {
+        return $this->invoice->findById($request);
 
         // $data = ['nombre'=>'alexis'];
         // $pdf = Pdf::loadView('pdf.factura', $data);
@@ -32,6 +41,7 @@ class PdfController extends Controller
         // return $pdf->stream();
 
         //return $pdf->download('factura.pdf');
+
     }
 
     public function recibo()
