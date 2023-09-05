1e1a043619a026a287b27492ae9dcd308e661687
alexisCobax <alexiscobax1@gmail.com>
2023-09-05 13:13:40 -0300

    [feature] genero controlares y servicios de pago

---
+++ app/Services/PagoWebService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\CarritoHelper;
+use App\Models\Banner;
+use Illuminate\Http\Request;
+use App\Helpers\PaginateHelper;
+use Illuminate\Http\Response;
+
+class PagoWebService
+{
+    public function findAll(Request $request)
+    {
+        // try {
+        //     $data = PaginateHelper::getPaginatedData($request, Banner::class);
+        //     return response()->json(['data' => $data], Response::HTTP_OK);
+        // } catch (\Exception $e) {
+        //     return response()->json(['error' => 'Ocurrió un error al obtener los productos'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // }
+    }
+
+    public function findById(Request $request)
+    {
+        // $data = Banner::find($request->id);
+
+        // return response()->json(['data' => $data], Response::HTTP_OK);
+    }
+
+    public function create(Request $request)
+    {
+
+        CarritoHelper::getCarrito();
+
+
+        $data = $request->all();
+        $banner = Banner::create($data);
+
+        if (!$banner) {
+            return response()->json(['error' => 'Failed to create Banner'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json($banner, Response::HTTP_OK);
+    }
+
+    public function update(Request $request)
+    {
+        // $banner = Banner::find($request->id);
+
+        // if (!$banner) {
+        //     return response()->json(['error' => 'Banner not found'], Response::HTTP_NOT_FOUND);
+        // }
+
+        // $banner->update($request->all());
+        // $banner->refresh();
+
+        // return response()->json($banner, Response::HTTP_OK);
+    }
+
+    public function delete(Request $request)
+    {
+        // $banner = Banner::find($request->id);
+
+        // if (!$banner) {
+        //     return response()->json(['error' => 'Banner not found'], Response::HTTP_NOT_FOUND);
+        // }
+
+        // $banner->delete();
+
+        // return response()->json(['id' => $request->id], Response::HTTP_OK);
+    }
+
+}
