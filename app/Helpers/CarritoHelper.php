11c36b41148c6d3fe8931aa9bc94f4c26e75c840
cynchro <alexissaucedo@gmail.com>
2023-09-18 19:07:55 -0300

    [feature] si no existe el carro en el helper devuelvo inexistente

--- app/Helpers/CarritoHelper.php
+++ app/Helpers/CarritoHelper.php
@@ -5,6 +5,7 @@ namespace App\Helpers;
 use App\Models\Carrito;
 use App\Models\Cliente;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class CarritoHelper
 {
@@ -15,6 +16,10 @@ class CarritoHelper
         $carrito = Carrito::where('cliente', $cliente->id)
             ->where('estado', 0)
             ->first();
+
+        if (!$carrito) {
+            return response()->json(['error' => 'Carrito inexistente'], Response::HTTP_NOT_FOUND);
+        }
 
         return [
             'id' => $carrito->id,
