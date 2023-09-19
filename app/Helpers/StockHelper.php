a377e811e1c9a07a8317d4960038aeb6e0b7914a
alexisCobax <alexiscobax1@gmail.com>
2023-09-19 11:14:39 -0300

    [feature] controlo stock cuando agrego

---
+++ app/Helpers/StockHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Producto;
+use Illuminate\Http\Response;
+
+class StockHelper
+{
+    public static function get($cantidad, $producto)
+    {
+        $producto = Producto::where('id',$producto)->first();
+
+        if ($cantidad > $producto->stock) {
+            return response()->json(['mensaje' => 'No hay stock suficiente', 'cantidad' => $producto->stock, 'status' => false], Response::HTTP_OK);
+        } else {
+            return response()->json(['mensaje' => '', 'cantidad' => $cantidad, 'status' => true], Response::HTTP_OK);
+        }
+    }
+}
