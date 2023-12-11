3ce3717b28afd4e3f9b91a0098e70e655cc9ed67
alexisCobax <alexiscobax1@gmail.com>
2023-12-11 15:17:15 -0300

    [feature] cambios sobre pedidos

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -86,6 +86,9 @@ class InvoiceService
             return response()->json(['error' => 'Failed to create Invoice'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
+        $pedido->invoice = $invoice->id;
+        $pedido->save();
+
         return response()->json($invoice, Response::HTTP_OK);
     }
 
