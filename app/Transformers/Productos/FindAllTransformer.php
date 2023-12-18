ff9a68d319be6f9630ae0bb6fcc12b8cfab51e47
alexisCobax <alexiscobax1@gmail.com>
2023-12-18 11:50:12 -0300

    [test] ñ

--- app/Transformers/Productos/FindAllTransformer.php
+++ app/Transformers/Productos/FindAllTransformer.php
@@ -14,7 +14,7 @@ class FindAllTransformer extends TransformerAbstract
         return [
             'id' => $producto->id,
             'imagenPrincipal' => $producto->imagenPrincipal . '.' . env('EXTENSION_IMAGEN_PRODUCTO'),
-            'nombre' => 'asdasdsad',
+            'nombre' => $producto->nombre,
             'codigo' => $producto->codigo,
             'categoria' => $producto->categoria,
             'categoriaNombre' => optional($producto->categorias)->nombre,
