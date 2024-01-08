37e5f29f34500af7a8743c44fdd5baadd1d7ee79
cynchro <alexissaucedo@gmail.com>
2024-01-08 15:17:54 -0300

    [fix] prospecto

--- app/Transformers/Invoices/CreateDetalleTransformer.php
+++ app/Transformers/Invoices/CreateDetalleTransformer.php
@@ -24,7 +24,7 @@ class CreateDetalleTransformer extends TransformerAbstract
                 $itemNumber = 'NN';
             }
 
-            $response[] = [
+            return [
                 'qordered' => $detalle->cantidad,
                 'qshipped' => $detalle->cantidad,
                 'qborder' => $detalle->cantidad,
@@ -34,19 +34,8 @@ class CreateDetalleTransformer extends TransformerAbstract
                 'netPrice' => $detalle->precio,
                 'invoice' => $id,
             ];
+        })->toArray();
 
-        //     return [
-        //         'qordered' => $detalle->cantidad,
-        //         'qshipped' => $detalle->cantidad,
-        //         'qborder' => $detalle->cantidad,
-        //         'itemNumber' => $itemNumber,
-        //         'Descripcion' => $descripcion,
-        //         'listPrice' => $detalle->precio,
-        //         'netPrice' => $detalle->precio,
-        //         'invoice' => $id,
-        //     ];
-        // })->toArray();
-    })->toArray();
         return $response;
     }
 }
