67ea39fbe5383aa114279300f1515a109a5c406d
cynchro <alexissaucedo@gmail.com>
2024-01-10 14:00:21 -0300

    [fecture] cotizacion  pdf cuando genero cotizacion + envio de email

--- app/Transformers/Pdf/FindByIdTransformer.php
+++ app/Transformers/Pdf/FindByIdTransformer.php
@@ -41,7 +41,7 @@ class FindByIdTransformer extends TransformerAbstract
                 'color' => optional($d->productos->colores)->id ?? '',
                 'precio' => number_format($d->precio, 2),
                 'total' => number_format($precio, 2),
-                'imagen' => env('URL_IMAGENES_PRODUCTOS') . optional($d->productos)->imagenPrincipal ?? '',
+                'imagen' => "https://phpstack-1091339-3819555.cloudwaysapps.com/storage/app/public/images/" . optional($d->productos)->imagenPrincipal ?? '',
             ];
         }
 
@@ -60,7 +60,7 @@ class FindByIdTransformer extends TransformerAbstract
                 'color' => '',
                 'precio' => number_format($dNn->precio, 2),
                 'total' => number_format($dNn->precio * $dNn->cantidad, 2),
-                'imagen' => env('URL_IMAGENES_PRODUCTOS') . 0,
+                'imagen' => "https://phpstack-1091339-3819555.cloudwaysapps.com/storage/app/public/images/" . 0,
             ];
         }
 
