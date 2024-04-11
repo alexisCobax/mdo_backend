e1a482f58b361d69f792b474910337227b207fc0
cynchro <alexissaucedo@gmail.com>
2024-04-11 17:43:23 -0300

    [feature] colores para la tienda

---
+++ app/Services/ColorWebService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\PaginateHelper;
+use App\Models\Color;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ColorWebService
+{
+    public function findAll(Request $request)
+    {
+        try {
+            $data = Color::all();
+
+            return response()->json(['data' => $data], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Ocurrió un error al obtener los productos'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function findById(Request $request)
+    {
+        $data = Color::find($request->id);
+
+        return response()->json(['data' => $data], Response::HTTP_OK);
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        $color = Color::create($data);
+
+        if (!$color) {
+            return response()->json(['error' => 'Failed to create Color'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json($color, Response::HTTP_OK);
+    }
+
+    public function update(Request $request)
+    {
+        $color = Color::find($request->id);
+
+        if (!$color) {
+            return response()->json(['error' => 'Color not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $color->update($request->all());
+        $color->refresh();
+
+        return response()->json($color, Response::HTTP_OK);
+    }
+
+    public function delete(Request $request)
+    {
+        $color = Color::find($request->id);
+
+        if (!$color) {
+            return response()->json(['error' => 'Color not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $color->delete();
+
+        return response()->json(['id' => $request->id], Response::HTTP_OK);
+    }
+}
