8522d8f0c5864a961bf637a42c39553045342044
cynchro <alexissaucedo@gmail.com>
2024-04-16 14:54:54 -0300

    [test] token

--- app/Services/PagoWebService.php
+++ app/Services/PagoWebService.php
@@ -32,8 +32,6 @@ class PagoWebService
         $pagoResponse = $pago->getContent();
         $pago = json_decode($pagoResponse);
 
-        dd($pago);die;
-
         /* Guardo Transaccion**/
         $this->saveTransaction($carrito['cliente'], json_encode([]), $pago->status, $pagoResponse);
 
