00a103edabff84a276e8432fe90385ce7cbd2687
alexisCobax <alexiscobax1@gmail.com>
2024-05-15 20:23:42 -0300

    [feature] top 10 clientes reportes

--- app/Services/ReportesService.php
+++ app/Services/ReportesService.php
@@ -360,4 +360,202 @@ UNION
             return response()->json(['error' => 'Ocurrió un error al obtener los invoices'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    public function topClientesList(Request $request)
+    {
+        $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+        $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+
+        $perPage = $request->input('cantidad', env('PER_PAGE'));
+        $page = $request->input('pagina', env('PAGE'));
+
+        $query = DB::table('invoice')
+            ->select(
+                'cliente.id',
+                'cliente.nombre',
+                DB::raw('SUM(invoice.subTotal) as total')
+            )
+            ->leftJoin('cliente', 'cliente.id', '=', 'invoice.cliente')
+            ->when($fecha_inicio && $fecha_fin, function ($query) use ($fecha_inicio, $fecha_fin) {
+                return $query->whereBetween('invoice.fecha', [$fecha_inicio, $fecha_fin]);
+            })
+            ->when($request->filled('nombreCliente'), function ($query) use ($request) {
+                return $query->where('cliente.nombre', 'like', '%' . $request->nombreCliente . '%');
+            })
+            ->groupBy('cliente.id', 'cliente.nombre')
+            ->orderBy('total', 'desc')
+            ->limit(10);
+        $results = $query->paginate($perPage, ['*'], 'page', $page);
+
+
+        $transformedResults = $results->map(function ($result) {
+            return [
+                'id' => $result->id,
+                'nombre' => $result->nombre,
+                'total' => $result->total
+            ];
+        });
+
+        $response = [
+            'data' => [
+                'headers' => [],
+                'original' => [
+                    'status' => Response::HTTP_OK,
+                    'total' => $results->total(),
+                    'cantidad_por_pagina' => $results->perPage(),
+                    'pagina' => $results->currentPage(),
+                    'cantidad_total' => $results->total(),
+                    'results' => $transformedResults,
+                ],
+                'exception' => null,
+            ],
+        ];
+
+        return response()->json($response);
+    }
+
+    public function topClientesReport(Request $request)
+    {
+        $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+        $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+        $nombreCliente = $request->filled('nombreCliente') ? $request->nombreCliente : null;
+
+        try {
+
+            $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+            $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+
+            $perPage = $request->input('cantidad', env('PER_PAGE'));
+            $page = $request->input('pagina', env('PAGE'));
+
+            $clientes = DB::table('invoice')
+                ->select(
+                    'cliente.id',
+                    'cliente.nombre',
+                    DB::raw('SUM(invoice.subTotal) as total')
+                )
+                ->leftJoin('cliente', 'cliente.id', '=', 'invoice.cliente')
+                ->when($fecha_inicio && $fecha_fin, function ($query) use ($fecha_inicio, $fecha_fin) {
+                    return $query->whereBetween('invoice.fecha', [$fecha_inicio, $fecha_fin]);
+                })
+                ->when($nombreCliente, function ($query) use ($nombreCliente) {
+                    return $query->where('cliente.nombre', 'like', '%' . $nombreCliente . '%');
+                })
+                ->groupBy('cliente.id', 'cliente.nombre')
+                ->orderBy('total', 'desc')->limit(10)->get()->toArray();
+
+            // Convertir los objetos stdClass en arrays asociativos
+            $clientes = array_map(function ($item) {
+                return (array) $item;
+            }, $clientes);
+
+            $cabeceras = [
+                'id', 'nombre', 'total'
+            ];
+
+            $response = ArrayToXlsxHelper::getXlsx($clientes, $cabeceras);
+
+            return $response;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Ocurrió un error al obtener los invoices'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function topMarcasList(Request $request)
+    {
+        $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+        $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+        $nombreMarca = $request->filled('nombreMarca') ? $request->nombreMarca : null;
+
+        $perPage = $request->input('cantidad', env('PER_PAGE'));
+        $page = $request->input('pagina', env('PAGE'));
+
+        $query = DB::table('pedidodetalle')
+        ->select('marcaproducto.id','marcaproducto.nombre as marca', DB::raw('SUM(pedidodetalle.cantidad) as cantidad'))
+        ->leftJoin('pedido', 'pedido.id', '=', 'pedidodetalle.pedido')
+        ->leftJoin('producto', 'producto.id', '=', 'pedidodetalle.producto')
+        ->leftJoin('marcaproducto', 'marcaproducto.id', '=', 'producto.marca')
+        ->when($fecha_inicio && $fecha_fin, function ($query) use ($fecha_inicio, $fecha_fin) {
+            return $query->whereBetween('pedido.fecha', [$fecha_inicio, $fecha_fin]);
+        })
+        ->when($nombreMarca, function ($query) use ($nombreMarca) {
+            return $query->where('marcaproducto.nombre', 'like', '%' . $nombreMarca . '%');
+        })
+        ->groupBy('marcaproducto.id','marcaproducto.nombre')
+        ->orderByDesc(DB::raw('SUM(pedidodetalle.cantidad)'))
+        ->limit(10);
+        $results = $query->paginate($perPage, ['*'], 'page', $page);
+
+
+        $transformedResults = $results->map(function ($result) {
+            return [
+                'id' => $result->id,
+                'marca' => $result->marca,
+                'cantidad' => $result->cantidad
+            ];
+        });
+
+        $response = [
+            'data' => [
+                'headers' => [],
+                'original' => [
+                    'status' => Response::HTTP_OK,
+                    'total' => $results->total(),
+                    'cantidad_por_pagina' => $results->perPage(),
+                    'pagina' => $results->currentPage(),
+                    'cantidad_total' => $results->total(),
+                    'results' => $transformedResults,
+                ],
+                'exception' => null,
+            ],
+        ];
+
+        return response()->json($response);
+    }
+
+    public function topMarcasReport(Request $request)
+    {
+        $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+        $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+
+        try {
+
+            $fecha_inicio = $request->filled('desde') ? $request->desde : null;
+            $fecha_fin = $request->filled('hasta') ? $request->hasta : null;
+            $nombreMarca = $request->filled('nombreMarca') ? $request->nombreMarca : null;
+    
+            $perPage = $request->input('cantidad', env('PER_PAGE'));
+            $page = $request->input('pagina', env('PAGE'));
+    
+            $marcas = DB::table('pedidodetalle')
+            ->select('marcaproducto.id','marcaproducto.nombre as marca', DB::raw('SUM(pedidodetalle.cantidad) as cantidad'))
+            ->leftJoin('pedido', 'pedido.id', '=', 'pedidodetalle.pedido')
+            ->leftJoin('producto', 'producto.id', '=', 'pedidodetalle.producto')
+            ->leftJoin('marcaproducto', 'marcaproducto.id', '=', 'producto.marca')
+            ->when($fecha_inicio && $fecha_fin, function ($query) use ($fecha_inicio, $fecha_fin) {
+                return $query->whereBetween('pedido.fecha', [$fecha_inicio, $fecha_fin]);
+            })
+            ->when($nombreMarca, function ($query) use ($nombreMarca) {
+                return $query->where('marcaproducto.nombre', 'like', '%' . $nombreMarca . '%');
+            })
+            ->groupBy('marcaproducto.id','marcaproducto.nombre')
+            ->orderByDesc(DB::raw('SUM(pedidodetalle.cantidad)'))
+            ->limit(10)->get()->toArray();
+
+            // Convertir los objetos stdClass en arrays asociativos
+            $marcas = array_map(function ($item) {
+                return (array) $item;
+            }, $marcas);
+
+            $cabeceras = [
+                'id', 'nombre', 'total'
+            ];
+
+            $response = ArrayToXlsxHelper::getXlsx($marcas, $cabeceras);
+
+            return $response;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Ocurrió un error al obtener los invoices'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
