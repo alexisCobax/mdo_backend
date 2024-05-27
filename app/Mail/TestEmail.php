86fd397a30cb4da658ff8ef3273d071293513a16
alexisCobax <alexiscobax1@gmail.com>
2024-05-27 16:53:38 -0300

    [test] dia1

--- app/Mail/TestEmail.php
+++ app/Mail/TestEmail.php
@@ -12,7 +12,7 @@ class TestEmail extends Mailable
 
     public function build()
     {
-        return $this->view('mdo.notificaciones.cotizaciones.dia3')
+        return $this->view('mdo.notificaciones.cotizaciones.dia5')
                     ->subject('Email de Prueba');
     }
 }
