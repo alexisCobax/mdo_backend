95747b9b40c0f09f87b8f6d7c1f96724822a7a44
alexisCobax <alexiscobax1@gmail.com>
2024-05-27 15:59:04 -0300

    [test] emails

--- resources/views/mdo/notificaciones/cotizaciones/dia1.blade.php
+++ resources/views/mdo/notificaciones/cotizaciones/dia1.blade.php
@@ -22,14 +22,16 @@
             Te contacto para consultarte si tuviste la oportunidad de revisar tu Cotización (te la enviamos ayer por email). Si tienes cualquier pregunta al respecto, por favor coméntanosla y con gusto te asistiremos.
             <br />
             Muchas gracias. <br></div>
+            <br /><br />
+            
         <div>
             <div>
                 <i>
                     <div style="text-align:center;display:inline"><span style="font-style:normal"><i><font face="georgia, serif" color="#000000">MDO INC | <u><a href="http://www.mayoristasdeopticas.net/" target="_blank">www.MayoristasDeOpticas.Net</a> </u></font></i></span></div>
                 </i><br>
             </div>
-            <div>
-                <i><font face="georgia, serif"><font color="#000000">2618 NW 112th Ave. Miami  FL 33172  USA</font><font style="color:rgb(0,0,0)"><br></font><font color="#000000">Horario de atención al cliente</font></font></i></div><div><font face="georgia, serif">Lunes a Viernes 8:30 a 16:30 Hrs. </font></div><div><font face="georgia, serif">PH: 305-513-9177 / 305-513-9191 / 305-513-9191.</font><span style="font-family:georgia,serif"> </span></div></div><div class="gmail_default" style="font-family:georgia,serif;font-size:large"></div><br><br><u></u>
+            @include('layouts.pie');
+            <div class="gmail_default" style="font-family:georgia,serif;font-size:large"></div><br><br><u></u>
  
 
  
