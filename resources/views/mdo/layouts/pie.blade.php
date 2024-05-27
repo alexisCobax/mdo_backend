896c83976fd2e91ea0d8ec3a22f5c2b42bccc897
alexisCobax <alexiscobax1@gmail.com>
2024-05-27 16:04:09 -0300

    [test] emails

--- resources/views/mdo/layouts/pie.blade.php
+++ resources/views/mdo/layouts/pie.blade.php
@@ -1,34 +1,33 @@
-
-<br/><br/>
+<br />
 <div>
-                <div style="text-align:center; display:inline;">
-                    <span style="font-style:normal;">
-                        <font face="georgia, serif" color="#000000">
-                            MDO INC | <u><a href="http://www.mayoristasdeopticas.net/" target="_blank">www.MayoristasDeOpticas.Net</a></u>
-                        </font>
-                    </span>
-                </div>
-                <br>
-                <div>
-                    <font face="georgia, serif" color="#000000">
-                        Domicilio: 2618 NW 112th Ave. Miami, FL, 33172. EE.UU.
-                        <br>
-                        Horario de atención al cliente
-                    </font>
-                </div>
-                <div>
-                    <font face="georgia, serif">
-                        Hora: 10:00 - 18:00, Lun - Vie
-                    </font>
-                </div>
-                <div>
-                    <font face="georgia, serif">
-                        Telefono: <a href="tel:+13055139177">+ 1 (305) 513-9177</a> / <a href="tel:+13055139191">+ 1 (305) 513-9191</a>
-                    </font>
-                </div>
-                <div>
-                    <font face="georgia, serif">
-                        Whatsapp:<a href="https://wa.me/13053168267">+ 1 305 316 8267</a>
-                    </font>
-                </div>
-            </div>
+    <div style="text-align:center; display:inline;">
+        <span style="font-style:normal;">
+            <font face="georgia, serif" color="#000000">
+                MDO INC | <u><a href="http://www.mayoristasdeopticas.net/" target="_blank">www.MayoristasDeOpticas.Net</a></u>
+            </font>
+        </span>
+    </div>
+    <br>
+    <div>
+        <font face="georgia, serif" color="#000000">
+            Domicilio: 2618 NW 112th Ave. Miami, FL, 33172. EE.UU.
+            <br>
+            Horario de atención al cliente
+        </font>
+    </div>
+    <div>
+        <font face="georgia, serif">
+            Hora: 10:00 - 18:00, Lun - Vie
+        </font>
+    </div>
+    <div>
+        <font face="georgia, serif">
+            Telefono: <a href="tel:+13055139177">+ 1 (305) 513-9177</a> / <a href="tel:+13055139191">+ 1 (305) 513-9191</a>
+        </font>
+    </div>
+    <div>
+        <font face="georgia, serif">
+            Whatsapp:<a href="https://wa.me/13053168267">+ 1 305 316 8267</a>
+        </font>
+    </div>
+</div>
--- resources/views/mdo/notificaciones/cotizaciones/dia1.blade.php
+++ resources/views/mdo/notificaciones/cotizaciones/dia1.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
- 
-<html xmlns="https://www.w3.org/1999/xhtml">
- 
- <head>
- 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
- 
-<title>Mayoristas De Opticas</title>
- 
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
- 
-</head>
- <body style="margin: 0; padding: 0;">
- <div dir="ltr">
-    <div class="gmail_default" style="font-family:georgia,serif;font-size:large">
-        <br>
-   </div>
-   <div class="gmail_quote">
 
-        <div class="gmail_default" style="font-family:georgia,serif">Hola, <BR />
-            Te contacto para consultarte si tuviste la oportunidad de revisar tu Cotización (te la enviamos ayer por email). Si tienes cualquier pregunta al respecto, por favor coméntanosla y con gusto te asistiremos.
-            <br />
-            Muchas gracias. <br></div>
+<html xmlns="https://www.w3.org/1999/xhtml">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <title>Mayoristas De Opticas</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+</head>
+
+<body style="margin: 0; padding: 0;">
+    <div dir="ltr">
+        <div class="gmail_default" style="font-family:georgia,serif;font-size:large">
+            <br>
+        </div>
+        <div class="gmail_quote">
+
+            <div class="gmail_default" style="font-family:georgia,serif">Hola, <BR />
+                Te contacto para consultarte si tuviste la oportunidad de revisar tu Cotización (te la enviamos ayer por email). Si tienes cualquier pregunta al respecto, por favor coméntanosla y con gusto te asistiremos.
+                <br />
+                Muchas gracias. <br>
+            </div>
             <br /><br />
-            @include('mdo.layouts.pie');
+            @include('mdo.layouts.pie')
 </body>
+
 </html>
