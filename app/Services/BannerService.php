37aaf9cc4eff2cfcf99ba1e702d512232606e824
alexisCobax <alexiscobax1@gmail.com>
2024-07-25 09:37:59 -0300

    [feature] sliders imagenes

--- app/Services/BannerService.php
+++ app/Services/BannerService.php
@@ -41,7 +41,7 @@ class BannerService
 
             return response()->json(['data' => $data], Response::HTTP_OK);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'Ocurrió un error al obtener la imagen'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
