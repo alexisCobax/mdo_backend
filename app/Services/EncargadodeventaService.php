ae538a839cf392bcd12162712749390289686704
alexisCobax <alexiscobax1@gmail.com>
2024-08-08 16:50:11 -0300

    [fix] solo si no estan suspendidos

--- app/Services/EncargadodeventaService.php
+++ app/Services/EncargadodeventaService.php
@@ -12,12 +12,33 @@ class EncargadodeventaService
     public function findAll(Request $request)
     {
         try {
-            $data = PaginateHelper::getPaginatedData($request, Encargadodeventa::class);
+            $data = $this->getPaginatedData($request, Encargadodeventa::class);
 
             return response()->json(['data' => $data], Response::HTTP_OK);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Ocurrió un error al obtener los productos'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public static function getPaginatedData($request, $model)
+    {
+        $page = $request->input('pagina', env('PAGE'));
+        $perPage = $request->input('cantidad', env('PER_PAGE'));
+
+        $data = $model::where('suspendido', 0)
+        ->orderBy('id', 'desc')
+        ->paginate($perPage, ['*'], 'page', $page);
+
+        $response = [
+            'status' => Response::HTTP_OK,
+            'total' => $data->total(),
+            'cantidad_por_pagina' => $data->perPage(),
+            'pagina' => $data->currentPage(),
+            'cantidad_total' => $data->total(),
+            'results' => $data->items(),
+        ];
+
+        return response()->json($response);
     }
 
     public function findById(Request $request)
