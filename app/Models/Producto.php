749276436a1842e47e1e341fd9db45a09ccf544e
alexisCobax <alexiscobax1@gmail.com>
2024-10-16 12:39:58 -0300

    marca producto

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -328,6 +328,7 @@ class Producto extends Model
 
     public function scopeRebajados($query)
 {
+    echo 1;die;
     $query->where(function ($query) {
         $query->where('precioPromocional', '>', 0)
               ->orWhere(function ($query) {
