dc96cb9936b749776d80ad9d18f6fc719744b1c1
alexisCobax <alexiscobax1@gmail.com>
2024-10-16 11:51:26 -0300

    marca producto

--- app/Filters/Productos/ProductosWebFilters.php
+++ app/Filters/Productos/ProductosWebFilters.php
@@ -91,13 +91,6 @@ class ProductosWebFilters
             ->orderBy('producto.ultimoIngresoDeMercaderia', 'desc')
             ->orderBy('producto.id', 'asc');
 
-            // Para ver la consulta SQL generada
-$sql = $query->toSql();
-$bindings = $query->getBindings();
-
-// Mostrar la consulta SQL y los bindings
-dd($sql, $bindings);
-
         $data = $query->paginate($perPage, ['*'], 'page', $page);
 
         // Crea una instancia del transformer
