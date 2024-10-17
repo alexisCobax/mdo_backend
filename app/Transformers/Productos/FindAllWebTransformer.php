bc514b7446fbf9370b3955016260b8e099027bac
alexisCobax <alexiscobax1@gmail.com>
2024-10-17 13:02:26 -0300

    arreglo precios rebajados 9.99 y menor a 20

--- app/Transformers/Productos/FindAllWebTransformer.php
+++ app/Transformers/Productos/FindAllWebTransformer.php
@@ -15,7 +15,7 @@ class FindAllWebTransformer extends TransformerAbstract
         if ($producto->imagenPrincipal != 0) {
             $imagen = Fotoproducto::where('id', $producto->imagenPrincipal)->first();
 
-            if ($imagen->url == NULL) {
+            if (isset($imagen->url) == NULL) {
                 $urlImagen = env('URL_IMAGENES_PRODUCTOS') . $imagen->id . '.jpg';
             } else {
                 $urlImagen = $imagen->url;
