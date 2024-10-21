12ce9fe5efd6723bace8c89c1185b7e235cd9d9d
alexisCobax <alexiscobax1@gmail.com>
2024-10-21 13:26:46 +0000

    token clover

--- app/Services/PagoWebService.php
+++ app/Services/PagoWebService.php
@@ -202,7 +202,8 @@ class PagoWebService
             $headers = [];
             $headers[] = 'Accept: application/json';
             //$headers[] = 'Authorization: Bearer 859c0171-ee8b-7c4b-7a07-3a02288fbc03';
-            $headers[] = 'Authorization: Bearer 557ccda4-98cb-5aa7-5ea5-39ad96096908';
+            //$headers[] = 'Authorization: Bearer 557ccda4-98cb-5aa7-5ea5-39ad96096908';
+            $headers[] = 'Authorization: Bearer 5382a5f0-10bb-87b9-c38f-ba0b5466ffa1';
             $headers[] = 'idempotency-key ' . $this->gen_uuid();
             $headers[] = 'Content-Type: application/json';
 
